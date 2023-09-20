696c0ea79a4e53423b9aaa138ff6eeb1435cd680
IgnacioGalceran <igna.galceran@gmail.com>
2023-09-20 00:51:19 -0300

    environment

--- Lab2023.Ej3.EF/Lab2023.Frontend.Angular/Ejercicio8/src/environment/environment.ts
+++
@@ -1,7 +0,0 @@
-export const env = {
-    production: false,
-    API_URL: 'https://localhost:44393/api/customer',
-    NOMINATIM_URL: 'https://nominatim.openstreetmap.org/search',
-    WEATHER_URL: 'https://api.weatherapi.com/v1',
-    WEATHER_KEY: 'b4f008b21480401b9dd124341231309'
-};
